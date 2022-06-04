74e9db8c2ec92ce636e2dc2bdbc51b6426c22795
Mustafabalkaya <balkayamustafa99@gmail.com>
2022-06-05 01:53:32 +0300

    C# Cift Bağlı Liste
    C# doubly linked list

---
+++ ciftbagliliste/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace ciftbagliliste
+{
+	partial class Form1
+	{
+		/// <summary>
+		///Gerekli tasarımcı değişkeni.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///Kullanılan tüm kaynakları temizleyin.
+		/// </summary>
+		///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer üretilen kod
+
+		/// <summary>
+		/// Tasarımcı desteği için gerekli metot - bu metodun 
+		///içeriğini kod düzenleyici ile değiştirmeyin.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button5 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(363, 40);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(150, 69);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "SONA EKLEME";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(140, 277);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(697, 190);
+			this.richTextBox1.TabIndex = 1;
+			this.richTextBox1.Text = "";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(121, 43);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 22);
+			this.textBox1.TabIndex = 2;
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(582, 176);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(255, 83);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "LİSTEYİ YAZDIR";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.Location = new System.Drawing.Point(363, 124);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(150, 55);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "DÜĞÜM  SİLME";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.Location = new System.Drawing.Point(363, 199);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(150, 60);
+			this.button4.TabIndex = 5;
+			this.button4.Text = "ARAYA EKLEME";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(121, 152);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 22);
+			this.textBox2.TabIndex = 6;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(121, 101);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 22);
+			this.textBox3.TabIndex = 7;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.label1.Location = new System.Drawing.Point(13, 43);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(55, 23);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "İSİM";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(12, 101);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(93, 23);
+			this.label2.TabIndex = 9;
+			this.label2.Text = "SOYİSİM";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(12, 155);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(95, 23);
+			this.label3.TabIndex = 10;
+			this.label3.Text = "NUMARA";
+			// 
+			// button5
+			// 
+			this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button5.Location = new System.Drawing.Point(593, 37);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(150, 69);
+			this.button5.TabIndex = 11;
+			this.button5.Text = "BAŞA EKLEME";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+			this.ClientSize = new System.Drawing.Size(1274, 493);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.button1);
+			this.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.Name = "Form1";
+			this.Text = "ÇİFT YÖNLÜ BAĞLI LİSTE";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button5;
+	}
+}
+
---
+++ ciftbagliliste/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ciftbagliliste
+{
+	public partial class Form1 : Form
+	{
+		public class Ciftdugum
+		{
+			public string isim;
+			public string soyisim;
+			public int no;
+			public Ciftdugum sonraki;
+			public Ciftdugum onceki;
+		}
+		Ciftdugum ilk = null;
+		Ciftdugum son = null;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)//sona ekleme
+		{
+			Ciftdugum yeni = new Ciftdugum();
+			yeni.isim = textBox1.Text;
+			yeni.no = Convert.ToInt32(textBox2.Text);
+			yeni.soyisim = textBox3.Text;
+			if (ilk == null)
+
+			{
+				ilk = yeni;
+				son = ilk;
+				ilk.onceki = null;
+				son.sonraki = null;
+
+			}
+			else
+			{
+				son.sonraki = yeni;
+				yeni.onceki = son;
+				son = yeni;
+				son.sonraki = null;
+
+			}
+
+		}
+		private void listeyiYazdir(Ciftdugum ilk)
+		{
+			richTextBox1.Text = null;
+			while (ilk != null)
+
+			{
+				richTextBox1.Text += ilk.isim + " : " + ilk.soyisim + " : " + ilk.no.ToString();
+				richTextBox1.Text += "<==>";
+				ilk = ilk.sonraki;
+
+			}
+			richTextBox1.Text += "null";
+		}
+
+		private void button2_Click(object sender, EventArgs e)//LİSTEYİ YAZDIR
+		{
+			listeyiYazdir(ilk);
+		}
+
+		private void button3_Click(object sender, EventArgs e)//silme işlemi
+		{
+			int no = Convert.ToInt32(textBox2.Text);
+			Ciftdugum silinecek = new Ciftdugum();
+			Ciftdugum onceki = new Ciftdugum();
+			silinecek = ilk;
+			if (ilk.no == no)
+			{
+				ilk = ilk.sonraki;
+				ilk.onceki = null;
+
+			}
+			else if (son.no==no)
+			{
+				son = son.onceki;
+				son.sonraki = null;
+			}
+			else
+			{
+				while (silinecek.no != no)
+				{
+					onceki = silinecek;
+					silinecek = silinecek.sonraki;
+				}
+				silinecek.onceki.sonraki = silinecek.sonraki;
+				silinecek.sonraki.onceki = silinecek.onceki;
+			}
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			Ciftdugum yeni = new Ciftdugum();
+			yeni.isim = textBox1.Text;
+			yeni.no = Convert.ToInt32(textBox2.Text);
+			yeni.soyisim = textBox3.Text;
+			Ciftdugum gecici = new Ciftdugum();
+			gecici = ilk;
+			if (ilk != null)
+
+			{
+				while (gecici.no < yeni.no)
+				{
+
+					if (gecici.sonraki.no > yeni.no)
+					{
+						break;
+					}
+					gecici = gecici.sonraki;
+				}
+				
+				gecici.sonraki.onceki = yeni;
+				yeni.sonraki = gecici.sonraki;
+				gecici.sonraki = yeni;
+				yeni.onceki = gecici;
+			}
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			Ciftdugum basaekle = new Ciftdugum();
+			basaekle.isim = Convert.ToString(textBox1.Text);
+			basaekle.soyisim = Convert.ToString(textBox3.Text);
+			basaekle.no = Convert.ToInt16(textBox2.Text);
+			ilk.onceki = basaekle;
+			basaekle.sonraki = ilk;
+			ilk = basaekle;
+			ilk.onceki = null;
+
+
+		}
+	}
+}
